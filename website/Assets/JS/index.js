5e5e7fe2eb9406df44be8ec511a67c652c93e7d7
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-10-09 11:20:35 +0100

    cards

--- website/Assets/JS/index.js
+++ website/Assets/JS/index.js
@@ -1,2 +1,17 @@
 // Scripts for index.html
 
+function hideElementOnSmallScreen() {
+    const elementToHide = document.querySelector('#hide'); // Change to the appropriate selector
+  
+    if (window.innerWidth <= 600) {
+      elementToHide.style.display = 'none';
+    } else {
+      elementToHide.style.display = 'block'; // Or 'initial' depending on your desired default state
+    }
+  }
+  
+  // Attach the event listener to the window's resize event
+  window.addEventListener('resize', hideElementOnSmallScreen);
+  
+  // Call the function initially to set the initial state
+  hideElementOnSmallScreen();
