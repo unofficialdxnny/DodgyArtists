995d6deff11b80ab609b91f344d7c2b9342a543f
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-10-29 01:00:59 +0100

    added spotify top songs from artist at random

--- website/Assets/JS/index.js
+++ website/Assets/JS/index.js
@@ -5,3 +5,16 @@ localStorage.setItem("lastPage", "index")
 
 // Video gallery
 
+// play song
+
+// const buttonElement = document.querySelector('button[data-testid="play-pause-button"]');
+
+// // Check if the button element exists
+// if (buttonElement) {
+//   // Delay for 3 seconds and then trigger a click event on the button element
+//   setTimeout(() => {
+//     buttonElement.click();
+//   }, 3000);
+// } else {
+//   console.log("Button element not found.");
+// }
